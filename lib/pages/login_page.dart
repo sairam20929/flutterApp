9236af93d28d9efccb420f3ed5a5a8ffb6ca7614
sai ram <sairam20929@gmail.com>
2022-06-18 12:56:22 +0530

    day3 completed

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Center(
+          child: Text(
+        "Login Page",
+        style: TextStyle(
+            color: Colors.blue, fontSize: 20, fontWeight: FontWeight.bold),
+      )),
+    );
+  }
+}
